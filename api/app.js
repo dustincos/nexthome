b048696e9355c42bb04eb5d477783db1af0a7752
Dustincos <cosmodust12@gmail.com>
2024-07-07 20:48:47 +0530

    Initial

---
+++ api/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
+const PORT = process.env.PORT
+const CLIENT_URL = process.env.CLIENT_URL
+
+const app = express();
+
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Your<br />Perfect Room Easily</h1>
+          <p>
+          Discover a wide range of listings tailored for you.<br />
+          Enjoy a hassle-free search experience with all the amenities you need.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>100+</h1>
+              <h2>Happy Customers</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Room Available</h2>
+            </div>
+            <div className="box">
+              <h1>100+</h1>
+              <h2>Property Sold</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
